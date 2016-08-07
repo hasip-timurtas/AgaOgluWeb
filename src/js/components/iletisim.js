d70c5ba075775ebb32c78969b8ba8bb4f8683a45
hasip <hasip.timurtas@gmail.com>
2016-08-07 14:47:18 +0300

    unused files deleted

--- src/js/components/iletisim.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Iletisim extends React.Component {
-    render() {
-        return (<h1>İletişim</h1>)
-    }
-}
--- src/js/components/kurumsal.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Kurumsal extends React.Component {
-    render() {
-        return (<h1>Kurumsal</h1>)
-    }
-}
